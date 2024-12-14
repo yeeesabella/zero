0da6fc87d56e8399f6b9498c04d516f5ff1dc22b
Isabella <54799586+yeeesabella@users.noreply.github.com>
2024-12-14 05:53:25 +0000

    update known bhs for 2025

--- sot.py
+++ sot.py
@@ -6,8 +6,8 @@ from datetime import datetime
 # BHS fixed at age 65
 # generate BHS table
 current_year = 2024
-years = list(range(current_year-8, current_year + 1))
-known_bhs = [49800, 52000, 54500, 57200, 60000, 63000, 66000, 68500, 71500]
+years = list(range(current_year-8, current_year + 2))
+known_bhs = [49800, 52000, 54500, 57200, 60000, 63000, 66000, 68500, 71500, 75500]
 
 data = {'year': years,
         'BHS': known_bhs
